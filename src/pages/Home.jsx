48ccdc8164cc3adde0cfbb433467ea5229b8dbb7
srikanth-nuthalapati <srikanthnuthalapati@outlook.com>
2025-01-28 20:31:03 +0530

    again

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ export default function Home() {
           <ScrollToTop />
           <Suggestions />
           <LoginSuggestion />
-          <Footer />
+          <Footer /> 
         </>
       )}
     </>
